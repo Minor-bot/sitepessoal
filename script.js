3ef96174381bb3859f8434d3324c78536a1b3f7e
Minor <caue.r.maia@gmail.com>
2025-07-15 00:45:56 -0300

    1 commit

---
+++ script.js
@@ -0,0 +1,7 @@
+  function abrirImagemCentralizada(id) {
+    document.getElementById(id).style.display = 'block';
+  }
+
+  function fecharImagemCentralizada() {
+    document.getElementById('imagemHyuncare').style.display = 'none';
+  }
